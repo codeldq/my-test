c91facbaf4a0ed06c1491cc08481520a6883408f
ldq <126954634+codeldq@users.noreply.github.com>
2023-07-10 09:55:39 +0800

    Update MyTestApplication.java
    test my january

--- src/main/java/com/example/mytest/MyTestApplication.java
+++ src/main/java/com/example/mytest/MyTestApplication.java
@@ -8,6 +8,7 @@ public class MyTestApplication {
 
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println("hello 7.10");
         SpringApplication.run(MyTestApplication.class, args);
     }
 
